8bab1781d2efdaf76ef5b0eed6fcec40cdedcb27
Luke Blaney <git@lukeblaney.co.uk>
2025-01-11 19:41:40 +0000

    add main nav back in, with correct links

--- src/inc/header.php
+++ src/inc/header.php
@@ -11,5 +11,12 @@
 			<a href="/">Luke's Semantic Web Sandbox</a>
 		</span>
 		<span id="about"></span>
+		<nav id="mainnav">
+			<ul>
+				<li<? if ($_SERVER['REQUEST_URI'] == "/") print ' class="currentpage"'; ?>><a href="/" title="Luke's Semantic Web Sandbox">Welcome</a></li>
+				<li<? if ($_SERVER['REQUEST_URI'] == "/theatre") print ' class="currentpage"'; ?>><a href="/theatre">Theatre Ontology</a></li>
+				<li<? if ($_SERVER['REQUEST_URI'] == "/audience") print ' class="currentpage"'; ?>><a href="/audience">Audience Ontology</a></li>
+			</ul>
+		</nav>
 	</header>
 	<main>
