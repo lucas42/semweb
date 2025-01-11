92c59ad3988ef069a9bda710f0bf95d75dff9b92
Luke Blaney <git@lukeblaney.co.uk>
2025-01-11 20:53:18 +0000

    use associative array for xmlns list, rather than object

--- src/inc/xmllibs.php
+++ src/inc/xmllibs.php
@@ -1,23 +1,25 @@
 <?php
 
-$xmlns->dc="http://purl.org/dc/elements/1.1/";
-$xmlns->dcterms="http://purl.org/dc/terms/";
-$xmlns->ev="http://purl.org/NET/c4dm/event.owl#";
-$xmlns->foaf="http://xmlns.com/foaf/0.1/";
-$xmlns->frbr="http://purl.org/vocab/frbr/core#";
-$xmlns->geo="http://www.w3.org/2003/01/geo/wgs84_pos#";
-$xmlns->mo="http://purl.org/ontology/mo/";
-$xmlns->po="http://purl.org/ontology/po/";
-$xmlns->owl="http://www.w3.org/2002/07/owl#";
-$xmlns->rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#";
-$xmlns->rdfs="http://www.w3.org/2000/01/rdf-schema#";
-$xmlns->thea="http://purl.org/theatre#";
-$xmlns->event="http://purl.org/NET/c4dm/event.owl#";
-$xmlns->xsd="http://www.w3.org/2001/XMLSchema#";
-$xmlns->participation="http://purl.org/vocab/participation/schema#";
-$xmlns->time="http://www.w3.org/2006/time#";
-$xmlns->omb="http://purl.org/ontomedia/ext/common/being#";
-$xmlns->vs="http://www.w3.org/2003/06/sw-vocab-status/ns#";
+$xmlns = [
+   'dc' => 'http://purl.org/dc/elements/1.1/',
+   'dcterms' => 'http://purl.org/dc/terms/',
+   'ev' => 'http://purl.org/NET/c4dm/event.owl#',
+   'foaf' => 'http://xmlns.com/foaf/0.1/',
+   'frbr' => 'http://purl.org/vocab/frbr/core#',
+   'geo' => 'http://www.w3.org/2003/01/geo/wgs84_pos#',
+   'mo' => 'http://purl.org/ontology/mo/',
+   'po' => 'http://purl.org/ontology/po/',
+   'owl' => 'http://www.w3.org/2002/07/owl#',
+   'rdf' => 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
+   'rdfs' => 'http://www.w3.org/2000/01/rdf-schema#',
+   'thea' => 'http://purl.org/theatre#',
+   'event' => 'http://purl.org/NET/c4dm/event.owl#',
+   'xsd' => 'http://www.w3.org/2001/XMLSchema#',
+   'participation' => 'http://purl.org/vocab/participation/schema#',
+   'time' => 'http://www.w3.org/2006/time#',
+   'omb' => 'http://purl.org/ontomedia/ext/common/being#',
+   'vs' => 'http://www.w3.org/2003/06/sw-vocab-status/ns#',
+];
 
 function print_rdf_head($rootxmlns) {
 	global $backend, $xmlns;
